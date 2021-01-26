fd39fcfc14d075dfbeed7b9587e9fcfc22b50a1c
theaharris <supershay84@gmail.com>
2021-01-25 22:04:10 -0500

    Validation for signup route done

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Firebase Firestore Documentation
 https://firebase.google.com/docs/firestore/query-data/get-data
+
+Reg Ex
+https://pastebin.com/f33g85pd
--- functions/index.js
+++ functions/index.js
@@ -65,6 +65,18 @@ app.post('/scream', (req, res) => {
     });
 });
 
+// HELPER FUNCTIONS
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+}
+
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+}
+
 // SIGNUP ROUTE
 app.post('/signup', (req,res) => {
     const newUser = {
@@ -74,9 +86,23 @@ app.post('/signup', (req,res) => {
         handle: req.body.handle
     };
 
+    let errors = {};
+    if(isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty'
+    } else if(!isEmail(newUser.email)) {
+        errors.email = 'Must use a valid email address'
+    }
+
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords do not match';
+    if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+// CHECK FOR ERRORS
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
 //VALIDATE DATA
+let token, userId;
 db.doc(`/users/${newUser.handle}`).get()
-    .then(doc => {
+    .then((doc) => {
         if(doc.exists){
             return res.status(400).json({ handle: 'This handle is already taken'});
         } else {
@@ -85,17 +111,31 @@ db.doc(`/users/${newUser.handle}`).get()
                 .createUserWithEmailAndPassword(newUser.email, newUser.password)
         }
     })
-    .then(data => {
+    .then((data) => {
+        userId = data.user.uid;
         return data.user.getIdToken();
     })
-    .then(token => {
+    .then((idToken) => {
+        token = idToken;
+        const userCredentials = {
+            handle: newUser.handle,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            userId
+        };
+        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
         return res.status(201).json({ token });
     })
     .catch (err => {
         console.error(err);
-        return res.status(500).json({ error: err.code});
+        if(err.code === 'auth/email-already-in-use'){
+            return res.status(400).json({ email: 'Email is already in use'});
+        } else {
+            return res.status(500).json({ error: err.code});
+        }
     });
-
 });
 
 
