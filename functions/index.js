5e6adb61f381fa1a37c5d89a8cf6a8b1d83c37ad
theaharris <supershay84@gmail.com>
2021-01-26 23:23:11 -0500

    Authentication done

--- functions/index.js
+++ functions/index.js
@@ -43,14 +43,42 @@ app.get('/screams', (req, res) => {
     })
     .catch((err) => console.error(err));
 });
-// CREATE
-app.post('/scream', (req, res) => {
+
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return res.status(403).json({error: "Unauthorized"});
+    }
+    admin.auth().verifyIdToken(idToken)
+    .then((decodedToken) => {
+        req.user = decodedToken;
+        console.log(decodedToken);
+        return db.collection('users')
+            .where('userId', "==", req.user.uid)
+            .limit(1)
+            .get();
+    })
+    .then((data) => {
+        req.user.handle = data.docs[0].data().handle;
+        return next();
+    })
+    .catch((err) => {
+        console.error('Verification Error', err);
+        return res.status(403).json(err);
+    })
+}
+
+// CREATE 
+app.post('/scream', FBAuth, (req, res) => {
     if(req.body.body.trim() === ''){
         return res.status(400).json({body: 'Must not be empty'});
     }
    const newScream = {
        body: req.body.body,
-       userHandle: req.body.userHandle,
+       userHandle: req.user.handle,
        createdAt: new Date().toISOString()
    };
    db
